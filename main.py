93fcd41d9cf1ca047c3dc5b30e79f9b3f7551447
3023029876 <azaliya_salikhova06@mail.ru>
2024-02-01 10:29:45 +0300

    8

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
 import pandas as pd
 import streamlit as st
 import plotly.express as px
+import openpyxl
 
 st.set_page_config(page_title="Excel Plotter")
 st.title("Excel Plotter 📈")
 st.subheader("Feed with your Excel file")
+uploaded_file = st.file_uploader("Выберете XLSX-файл", type="xlsx")
+if uploaded_file:
+    st.markdown('---')
+    df = pd.read_excel(uploaded_file, engine='openpyxl')
+    st.dataframe(df)
